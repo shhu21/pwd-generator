f107081891d98a53c1c579efeba0c57ac5288414
shhu21 <hu.sara21@gmail.com>
2020-06-03 00:44:21 -0700

    adds comments

--- script.js
+++ script.js
@@ -1,30 +1,5 @@
 // Assignment code here
-/*
-*could make pwd an object
-
-push checkedOptions into an array (always add regular characters too)
-arr characters (strings of each set of special characters)
-int totalLength of pwd
-int tempLength = totalLength
-string pwd = "";
-
-*could be done through recursion with the while condition as the base condition
-while(pwd.length() != totalLength)
-{
-  numOption.clear();
-  for checkedOptions {
-    int num push random(1, tempLength - (checkOptions.size() - 1))
-    tempLength -= num
-    
-    for num {
-      pwd += characters[random(0, characters.size() - 1)];
-    }
-  }
-}
-
-document.get = pwd;
- */
-
+// password object declaration
 var password = {
   pwd: "",
   len: 0,
@@ -32,6 +7,7 @@ var password = {
   numOptions: 0
 };
 
+// updates count of chosen criteria
 function ifOption(opt) {
   if(opt){
     password.numOptions++;
@@ -39,17 +15,21 @@ function ifOption(opt) {
 };
 
 function setConditions() {
+  // initialize password values
   password.pwd = "";
   password.len = 0;
   password.options = [];
   numOptions = 0;
   
+  // get the length from the user
   password.len = parseInt(window.prompt("Please enter a password length within the range [8, 128]."));
+  // check if the length meets the requirements
   if(password.len < 8 || password.len > 128) {
     window.prompt("Please choose a password length between 8 and 128.");
     setConditions();
   }
 
+  // get the criteria
   window.alert("Please choose your password criteria.");
   var opt = window.confirm("Would you like to include lowercase letters?");
   password.options.push(opt);
@@ -64,27 +44,41 @@ function setConditions() {
   password.options.push(opt);
   ifOption(opt);
 
+  // criteria check
   if(password.numOptions == 0) {
     window.prompt("Please select at least one criteria.");
     setConditions();
   }
 };
 
+// generates the random password
 function generatePassword() {
+  // strings of each criteria
   var characters = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789", " !\"#\$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
   
+  // set the criteria conditions
   setConditions();
+  // set temporary length
   var tempLen = password.len;
+
+  // while the password length is less than the set length
   while(password.pwd.length < password.len) {
+    // loops through the character criteria
     for(var i = 0; i < password.options.length; i++) {
+      // checks if is a chosen criteria
       if(password.options[i]) {
+        // frequency of the chosen criteria
         var num = tempLen;
+        // the temporary length needs to be greater than the number of criteria chosen or it'll break the max limit on random (becomes a negative number)
         if(tempLen > password.numOptions) {
           num = Math.floor(Math.random() * (tempLen - password.numOptions)) + 1;
         }
+        // deduct the random length from the temporary length (length left to generate)
         tempLen -= num;
+        // will break if num is larger than the characters strings
+        // generate a string of length num by randomizing the indices from the chosen criteria strings
         for(var j = 0; j < num; j++){
-          // add a random might need another for loop
+          // TODO: randomize
           password.pwd += characters[i][j];
         }
       }
@@ -92,7 +86,7 @@ function generatePassword() {
   }
 
   console.log(password.pwd.length);
-  //store in a temp string and then randomize
+  // TODO: store in a temp string and then randomize
   return password.pwd;
 };
 
